b90cd1b8a5621e82dee0c6640e5f296ef716e625
Pamela Fellers <pamela.fellers@zybooks.com>
2022-12-22 15:45:07 -0600

    Update NormalCalculator.py

--- NormalCalculator.py
+++ NormalCalculator.py
@@ -93,12 +93,16 @@ with col2:
             ax.plot(x2, y2, color='tab:blue')
             ax.fill_between(x2, 0, y2, color='tab:orange', alpha=0.6)
             ax.plot((value1, value1), (-.02, norm.pdf(value1, loc=meanmu, scale=stsigma)), scaley = False, color='tab:orange')
+            probcalc = norm.cdf(value1,loc=meanmu, scale=stsigma)
+            text="P(X < %0.2f) = %0.2f" %(value1, probcalc) 
         elif probability == "P(X > value)":     
             x2 = np.linspace(value1, norm.ppf(0.9999, meanmu, stsigma), 100)
             y2 = norm.pdf(x=x2, loc=meanmu, scale=stsigma)
             ax.plot(x2, y2, color='tab:blue')
             ax.fill_between(x2, 0, y2, color='tab:orange', alpha=0.6)
             ax.plot((value1, value1), (-.02, norm.pdf(value1, loc=meanmu, scale=stsigma)), scaley = False, color='tab:orange')
+            probcalc = 1-norm.cdf(value1,loc=meanmu, scale=stsigma)
+            text="P(X > %0.2f) = %0.2f" %(value1, probcalc)
         else:     
             x2 = np.linspace(value1, value2, 100)
             y2 = norm.pdf(x=x2, loc=meanmu, scale=stsigma)
@@ -106,6 +110,8 @@ with col2:
             ax.fill_between(x2, 0, y2, color='tab:orange', alpha=0.6)
             ax.plot((value1, value1), (-.02, norm.pdf(value1, loc=meanmu, scale=stsigma)), scaley = False, color='tab:orange')
             ax.plot((value2, value2), (-.02, norm.pdf(value2, loc=meanmu, scale=stsigma)), scaley = False, color='tab:orange')
+            probcalc = norm.cdf(value2,loc=meanmu, scale=stsigma)-norm.cdf(value1,loc=meanmu, scale=stsigma)
+            text="P(%0.2f < X < %0.2f) = %0.2f" %(value1, value2, probcalc)
     else:
         fig, ax = plt.subplots()
         x = np.linspace(norm.ppf(0.0001, meanmu, stsigma), norm.ppf(0.9999, meanmu, stsigma), 100)
@@ -114,5 +120,6 @@ with col2:
         title1=('normal(%0.2f, %0.2f)' %(meanmu, stsigma))
         ax.title.set_text(title1)
         plt.ylim(bottom=0) 
+        text=""
     st.pyplot(fig)
-    
+    st.write(text)
