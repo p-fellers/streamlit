ce611977ef2edb54346abc08ad302c3abed1a14a
Pamela Fellers <pamela.fellers@zybooks.com>
2023-01-27 13:53:46 -0600

    Update tCalculator.py

--- tCalculator.py
+++ tCalculator.py
@@ -170,3 +170,25 @@ with col2:
     st.pyplot(fig)
     st.markdown(f"""<div style='text-align: center'>{text}</h1>""", unsafe_allow_html=True)
 
+    check2 = st.checkbox("Show description")
+    if check2:
+        mean1=meanmu
+        stdev1=(df/(df-2))**0.5
+        alttext1= "Probability distribution curve of a %s distribution. The distribution \
+        is unomodal, symmetric, and bell-shaped with a mean of %0.2f and a standard deviation \
+        of %0.2f." %(title1, mean1, stdev1)
+
+        if calculate == "Probability given value":
+            alttext2 = "The calculated probability is %s." %(text)
+        elif calculate == "Value given probability":
+            if tail == "lower tail":
+                alttext2 = "The value such that P( X < value) = %0.3f is %0.3f, %s." %(prob, valresult, text)
+            elif tail == "upper tail":
+                alttext2 = "The value such that P( X > value) = %0.3f is %0.3f, %s." %(prob, valresult, text)
+            else:
+                alttext2 = "The middle %0.3f of the distribution lies between %0.3f and %0.3f, %s." %(prob, valresult1, valresult2, text)
+        else:
+            alttext2=""
+        st.write(alttext1)
+        st.write(alttext2)
+
